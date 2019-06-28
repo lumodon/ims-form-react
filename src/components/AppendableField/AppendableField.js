f73449ca7743387a280aca77c778d89e8e91cfaa
Serafin Wesnidge <leaf466@gmail.com>
2019-06-28 12:59:45 -0700

    fix: Dynamic Appendable form keeps state
    State no longer disappears when view is closed
    State now managed by parent component
    Still needs styling

--- src/components/AppendableField/AppendableField.js
+++ src/components/AppendableField/AppendableField.js
@@ -4,18 +4,16 @@ import { makeStyles } from '@material-ui/styles'
 import Paper from '@material-ui/core/Paper'
 import IconButton from '@material-ui/core/IconButton'
 import PlusSymbol from '@material-ui/icons/AddCircleOutline'
-import DropDownIcon from '@material-ui/icons/ArrowDropDownCircle'
-import RemoveIcon from '@material-ui/icons/RemoveCircle'
-
 import Switch from '@material-ui/core/Switch'
 import Collapse from '@material-ui/core/Collapse'
+
+import FieldListItem from './FieldListItem'
 
 const useStyles = makeStyles(() => ({
   root: {
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
-    width: 400,
   },
   iconButton: {
     padding: 10,
@@ -29,103 +27,148 @@ const useStyles = makeStyles(() => ({
   flexColumn: {
     flexDirection: 'column',
   },
+  container: {
+    display: 'flex',
+    flex: '1 0 auto',
+    alignSelf: 'flex-start',
+  },
   flex: {
     display: 'flex',
   },
-  flexEnd: {
+  flexStart: {
     alignSelf: 'flex-start',
-    flex: '1 0 auto',
-  },
-  minWidth: {
-    minWidth: 300,
   },
   paper: {
     margin: 10,
   },
+  collapsable: {
+    width: '100%',
+  },
   customSwitch: {
-    transform: 'translateX(15px)',
+    transform: 'translateX(2px)',
     marginBottom: -15,
+    alignSelf: 'center',
   }
 }))
 
-/**
- * @function :  dataReducer
- * @param {object} currentState - Contain initial and final state of data
- * @param {object[]} currentState.listOfFields
- *   - List of all fields component will render
- *   - field should include { name: <string> } as unique identifier
- * @param {object} action - Payload
- * @param {('appendList'|'removeFromList')} action.type - List of actions
- * @param {object} action.newField - appendList: List of new listOfFields
- * @param {string} action.fieldName - removeFromList: Field name to be removed
- */
 const reducer = (currentState, action) => {
-  switch (action.type) {
-    case 'appendList':
-      return {listOfFields: [...currentState.listOfFields, action.newField]}
-    case 'removeFromList':
-      const modifiedList = currentState.listOfFields
-        .filter(field => field.name !== action.fieldName)
-      return {listOfFields: [...modifiedList]};
-    default:
-      throw new Error();
-  }
+  console.log('currentstate: ', currentState, '\naction:\n', action)
+  const caseSwitch = {
+    'appendList': () => {
+      const newList = {
+        listOfFields: {
+          ...currentState.listOfFields,
+          [action.newField.name]: {value: ''}
+        }
+      }
+      console.log('newlist: ', newList)
+      return newList
+    },
+    'removeFromList': () => {
+      console.log('Removing ', action.fieldName, ' from ', currentState.listOfFields)
+      const modifiedList = {...currentState.listOfFields}
+      delete modifiedList[action.fieldName]
+      return {listOfFields: modifiedList}
+    },
+    'setValue': () => {
+      const modifiedList = {...currentState.listOfFields}
+      modifiedList[action.fieldName].value = action.value
+      return {listOfFields: modifiedList}
+    }
+  }[action && action.type]
+  if(caseSwitch) return caseSwitch()
+  else throw new Error()
 }
 
 const initialState = {
-  'listOfFields': [{name: 0}]
+  'listOfFields': {'0': {value: ''}}
 }
+
+let incrementor = 1 // 0 reserved for undeletable list item
 
 export default function AppendableField({ name, label }) {
   const [state, dispatch] = useReducer(reducer, initialState)
   const [listDisplayed, setListDisplayed] = useState(true)
+  const [controlledValue, setControlledValue] = useState('')
+
+  console.log('initialstate:', state)
+
   const classes = useStyles()
 
   const addEmpty = () => {
     dispatch({
       type: 'appendList',
       newField: {
-        name: state.listOfFields.length
+        name: incrementor++
       }
     })
   }
 
-  const removeItem = useCallback(key => () => {
+  const removeItem = useCallback(key => {
+    console.log('callback made INSIDE on ', key)
     dispatch({type: 'removeFromList', fieldName: key})
-  }, [dispatch])
-
-  const fieldListDisplay = () => {
-    if(listDisplayed) {
-      return 
-    }
-    return 
-  }
+  }, [state, dispatch])
 
   const toggleList = useCallback(() => {
       setListDisplayed(prev => !prev)
     },
-    [setListDisplayed],
+    [listDisplayed, setListDisplayed],
   )
 
+  const reducerSetValue = useCallback((value, fieldName) => {
+    console.log('reducer value: ', value, '\nreducer fieldname: ', fieldName)
+    return dispatch({type: 'setValue', value, fieldName})
+  }, [state])
+
+  const mapListOfFields = useCallback(cb => {
+    const list = []
+    for(const key in state.listOfFields) {
+      list.push(cb(key))
+    }
+    return list
+  }, [state])
+
   return (
-    <Paper className={`${classes.root} ${classes.flex}`}>
+    <Paper className={classes.root}>
       <div
         style={{paddingBottom: '10px'}}
-        className={clsx(classes.flexColumn, classes.flex, classes.minWidth)}
+        className={classes.container}
       >
-        <Collapse in={listDisplayed} out={String(!listDisplayed)} collapsedHeight="60px">
+        <Collapse
+          in={listDisplayed}
+          out={String(!listDisplayed)}
+          collapsedHeight="60px"
+          className={classes.collapsable}
+        >
           <Paper elevation={3} className={classes.paper}>
             {
               listDisplayed
-              ? state.listOfFields.map(({}, fieldIndex) => (
-                <FieldListItem key={name + String(fieldIndex)} fieldIndex={fieldIndex} />
-              ))
-              : <FieldListItem fieldIndex={0} removeItem={removeItem} />
+              ? mapListOfFields(key => {
+                  return (
+                    <FieldListItem
+                      name={name + String(key)}
+                      key={key}
+                      fieldIndex={key}
+                      removeItem={removeItem}
+                      label={label}
+                      setControlledValue={reducerSetValue}
+                      controlledValue={state.listOfFields[key].value}
+                    />
+                  )
+                })
+              : <FieldListItem
+                name={name + '0'}
+                fieldIndex="0"
+                removeItem={removeItem}
+                label={label}
+                setControlledValue={reducerSetValue}
+                controlledValue={state.listOfFields['0'].value}
+              />
             }
           </Paper>
         </Collapse>
       </div>
-      <div className={clsx(classes.flexColumn, classes.flex, classes.flexEnd)}>
+      <div className={clsx(classes.flexColumn, classes.flex, classes.flexStart)}>
         <Switch
           classes={{
             switchBase: classes.iOSSwitchBase,
--- src/components/AppendableField/FieldListItem.js
+++ src/components/AppendableField/FieldListItem.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Field } from 'react-final-form'
-import { TextField } from 'final-form-material-ui'
 import { makeStyles } from '@material-ui/styles'
+import IconButton from '@material-ui/core/IconButton'
+import RemoveIcon from '@material-ui/icons/RemoveCircle'
+import clsx from 'clsx'
 
 const useStyles = makeStyles(() => ({
   input: {
@@ -15,29 +17,65 @@ const useStyles = makeStyles(() => ({
   flex: {
     display: 'flex',
   },
+  customTextField: {
+    fontSize: '1.2em',
+  },
+  heightLimit: {
+    height: 50,
+  }
 }))
 
-export default function FieldListItem({ fieldIndex, name, removeItem }) {
+function CustomTextField({ label, controlledValue, onValueChange }) {
   const classes = useStyles()
 
-  const RemoveIconRender = () => (
-    fieldIndex > 0 && (
-      <IconButton onClick={removeItem(fieldIndex)} className={classes.iconButton}>
-        <RemoveIcon />
-      </IconButton>
-    )
+  return (
+    <input
+      value={controlledValue}
+      className={clsx(classes.customTextField, classes.heightLimit)}
+      placeholder={label}
+      onChange={onValueChange}
+    ></input>
+  )
+}
+
+/**
+ * @param {string} fieldIndex - Key
+ * @param {string} name - Form name for input field
+ * @param {string} label - Display label
+ * @param {function} removeItem - Callback for clicking delete button
+ */
+export default function FieldListItem({ fieldIndex, name, label, removeItem, setControlledValue, controlledValue }) {
+  const classes = useStyles()
+
+  const useRemoveItem = useCallback(() => {
+      setControlledValue('', fieldIndex)
+      removeItem(fieldIndex)
+    },
+    [fieldIndex],
   )
 
+  const changeHandler = useCallback((e) => {
+    setControlledValue(e.target.value, fieldIndex)
+    return controlledValue
+  }, [controlledValue])
+
   return (
-    <div className={classes.flex}>
+    <div className={clsx(classes.flex, classes.heightLimit)}>
       <Field
-        className={classes.input}
+        className={clsx(classes.input, classes.heightLimit)}
         fullWidth
         name={name}
-        component={TextField}
+        component={CustomTextField}
         label={label}
+        controlledValue={controlledValue}
+        onValueChange={changeHandler}
+        withRef
       />
-      {RemoveIconRender}
+      {fieldIndex > 0 && (
+        <IconButton onClick={useRemoveItem} className={classes.iconButton}>
+          <RemoveIcon />
+        </IconButton>
+      )}
     </div>
   )
 }
